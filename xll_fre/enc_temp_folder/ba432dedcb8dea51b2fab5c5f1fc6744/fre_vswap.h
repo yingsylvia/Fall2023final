7d119f3fd9c06b24b4a012ff1599fa18f18d89c9
“yingsylviagit config --list | grep user.namegit config --global user.email “yj2911@nyu.edu <yj2911@nyu.edu>
2023-12-21 17:22:41 -0500

    final

---
+++ xll_fre/enc_temp_folder/ba432dedcb8dea51b2fab5c5f1fc6744/fre_vswap.h
@@ -0,0 +1,69 @@
+﻿#pragma once
+#include <algorithm>
+#include <vector>
+#include <cmath> 
+#include "../xll/xll/ensure.h"
+
+namespace fre::vswap {
+
+	// Piecewise linear curve determined by (x_i, y_i) for i = 0, ..., n-1
+	class pwlinear {
+		std::vector<double> x;
+		std::vector<double> y;
+		// x[i] <= x_ < x[i+1]
+		size_t index(double x_) const
+		{
+			return std::lower_bound(x.begin(), x.end(), x_) - x.begin();
+		}
+	public:
+		pwlinear(size_t n, const double* x_, const double* y_)
+			: x(x_, x_ + n), y(y_, y_ + n)
+		{
+			ensure(n >= 2);
+			ensure(std::is_sorted(x.begin(), x.end()));
+		}
+		// value at x
+		double value(double x_) const
+		{
+			size_t i = index(x_);
+
+			if (i == x.size() - 1) {
+				ensure(x_ > x.back());
+				--i; // use last two points
+			}
+			double m = (y[i + 1] - y[i]) / (x[i + 1] - x[i]);
+
+			return y[i] + m * (x_ - x[i]);
+		}
+
+		// first derivative at x
+		double derivative(double x_) const {
+			size_t i = index(x_);
+			if (i == x.size() - 1) {
+				--i;
+			}
+
+			double m = (y[i + 1] - y[i]) / (x[i + 1] - x[i]);
+			return m;
+		}
+
+		// second derivative at x[1], ..., x[n-2]
+		std::vector<double> delta() const {
+			std::vector<double> deltas(x.size() - 1, 0.0);
+			return deltas;
+		}
+		double variance(double f, size_t n, const double* k, const double* p, const double* c) const {
+			double var = 0.0;
+			for (size_t i = 0; i < n; ++i) {
+				if (k[i] <= 0) {
+					throw std::invalid_argument("Strike price cannot be zero or negative");
+				}
+				double term = -2 * std::log(k[i] / f) + 2 * (k[i] - f) / f;
+				double value = (k[i] < f) ? p[i] : c[i];
+				var += value * term;
+			}
+			return var / n;
+		}
+	};
+
+} // namespace fre::vswap
---
+++ xll_fre/enc_temp_folder/de26b8157726f165341d29e8aa9fa518/xll_vswap.cpp
@@ -0,0 +1,34 @@
+﻿#include "fre_vswap.h"
+#include "xll_fre.h"
+
+using namespace xll;
+using namespace fre::vswap;
+
+// Implement xll_vswap
+AddIn xai_vswap(
+    Function(XLL_DOUBLE, "xll_vswap", "XLL.VSWAP")
+    .Arguments({
+        Arg(XLL_DOUBLE, "f", "is the forward price."),
+        Arg(XLL_FP, "k", "is an array of strike prices."),
+        Arg(XLL_FP, "p", "is an array of put prices corresponding to strikes."),
+        Arg(XLL_FP, "c", "is an array of call prices corresponding to strikes."),
+        })
+        .Category("VSWAP")
+    .FunctionHelp("Calculate variance for variance swap.")
+);
+
+double WINAPI xll_vswap(double f, const _FP12* k, const _FP12* p, const _FP12* c) {
+#pragma XLLEXPORT
+   
+    ensure(k->rows == p->rows && p->rows == c->rows);
+
+   
+    std::vector<double> x_values(k->rows, 1.0); 
+    std::vector<double> y_values(k->rows, 1.0); 
+
+ 
+    pwlinear pl(k->rows, x_values.data(), y_values.data());
+
+
+    return pl.variance(f, k->rows, k->array, p->array, c->array);
+}
